08d96eba4802d2788c5179248637a94f749ab22b
Gajanan Wattamwar <wattamwargaj@gmail.com>
2022-01-23 21:39:15 +0530

    count

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   const [show, setShow] = useState(true);
   return (
     <div className="App">
-      {show ? <Timer start={5} end={7} /> : null}
+      {show ? <Timer start={0} end={10} /> : null}
       <button onClick={() => setShow(!show)}>{show ? "Hide" : "Show"}</button>
     </div>
   );
